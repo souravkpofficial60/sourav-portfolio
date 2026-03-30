7c3e0b24fa4dd41afadf93143a3c61ce7ccedf59
souravkpofficial60 <souravkpofficial60@gmail.com>
2026-03-31 03:49:15 +0530

    fixed no redirect form

--- script.js
+++ script.js
@@ -2,34 +2,33 @@ console.log("JS WORKING");
 
 const form = document.getElementById("contactForm");
 const status = document.getElementById("formStatus");
+const btn = document.getElementById("submitBtn");
 
-if (form) {
-  form.addEventListener("submit", function(e) {
-    e.preventDefault(); // 🚨 THIS STOPS REFRESH
+btn.addEventListener("click", function () {
 
-    status.textContent = "Sending...";
+  status.textContent = "Sending...";
 
-    fetch("https://portfolio-backend-y3um.onrender.com/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name: form.name.value,
-        email: form.email.value,
-        message: form.message.value
-      })
+  fetch("https://portfolio-backend-y3um.onrender.com/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value
     })
-    .then(res => {
-      if (res.ok) {
-        status.textContent = "Message sent successfully ✅";
-        form.reset();
-      } else {
-        status.textContent = "Error sending ❌";
-      }
-    })
-    .catch(() => {
-      status.textContent = "Server error ❌";
-    });
+  })
+  .then(res => {
+    if (res.ok) {
+      status.textContent = "Message sent successfully ✅";
+      form.reset();
+    } else {
+      status.textContent = "Error sending ❌";
+    }
+  })
+  .catch(() => {
+    status.textContent = "Server error ❌";
   });
-}
+
+});
