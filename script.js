861e62854d2c4a492940dda0a8b437e9fd39843e
souravkpofficial60 <souravkpofficial60@gmail.com>
2026-03-31 04:03:22 +0530

    fixed form no redirect final

--- script.js
+++ script.js
@@ -4,31 +4,35 @@ const form = document.getElementById("contactForm");
 const status = document.getElementById("formStatus");
 const btn = document.getElementById("submitBtn");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", async function () {
 
   status.textContent = "Sending...";
 
-  fetch("https://portfolio-backend-y3um.onrender.com/contact", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      name: form.name.value,
-      email: form.email.value,
-      message: form.message.value
-    })
-  })
-  .then(res => {
+  try {
+    const res = await fetch("https://portfolio-backend-y3um.onrender.com/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: form.name.value,
+        email: form.email.value,
+        message: form.message.value
+      })
+    });
+
     if (res.ok) {
       status.textContent = "Message sent successfully ✅";
+      status.style.color = "lime";
       form.reset();
     } else {
       status.textContent = "Error sending ❌";
+      status.style.color = "red";
     }
-  })
-  .catch(() => {
+
+  } catch (error) {
     status.textContent = "Server error ❌";
-  });
+    status.style.color = "red";
+  }
 
 });
